a798b95a8f33b13133012a5b719e1da9976d2fc8
Alfin Jones <153479672+URK23CS1190@users.noreply.github.com>
2026-01-31 14:42:22 +0530

    fix: correct test import path

--- test/test_app.py
+++ test/test_app.py
@@ -1,6 +1,8 @@
 import sys
 import os
-sys.path.append(os.path.dirname(os.p[ath.dirname(_file_)]))
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from app import add
 
 
